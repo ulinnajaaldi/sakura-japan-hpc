d51b1c0e8b3e4020be309d2bb0043cfc8f378a0f
Ulinnaja Aldi <ulinnajaaldi24@gmail.com>
2023-05-23 23:23:06 +0700

    feat: create navbar, hero element and about section

--- tailwind.config.js
+++ tailwind.config.js
@@ -5,14 +5,21 @@ module.exports = {
     extend: {
       colors: {
         accent: "#FD636B",
+        primary: "#0c1117",
+        secondary: "#FD636B",
       },
       screens: {
-        "2xl": "133px",
+        "2xl": "1439px",
       },
     },
     container: {
       center: true,
-      padding: "6rem",
+      padding: {
+        DEFAULT: "1rem",
+        sm: "2rem",
+        md: "3rem",
+        xl: "6rem",
+      },
     },
   },
   plugins: [],
